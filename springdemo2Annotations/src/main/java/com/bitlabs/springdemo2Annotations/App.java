0d30e66ebf91940f45deb21f1e819dca861738fe
kumarbitlabs <108260961+kumarbitlabs@users.noreply.github.com>
2023-01-27 15:44:06 +0530

    Add files via upload

---
+++ springdemo2Annotations/src/main/java/com/bitlabs/springdemo2Annotations/App.java
@@ -0,0 +1,20 @@
+package com.bitlabs.springdemo2Annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com\\bitlabs\\springdemo2Annotations\\config.xml");
+
+		Emp em = context.getBean("employee", Emp.class);
+		System.out.println(em.hashCode());
+
+		
+		Emp em2 = context.getBean("employee", Emp.class);
+		System.out.println(em2.hashCode());
+	}
+
+}
---
+++ springdemo2Annotations/src/test/java/com/bitlabs/springdemo2Annotations/Springdemo2AnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitlabs.springdemo2Annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdemo2AnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
